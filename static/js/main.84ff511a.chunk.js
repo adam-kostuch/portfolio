1a2491f235a584efed17f2b8564dc6a66fefdfd6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-17 23:03:00 +0000

    Deploy adam-kostuch/portfolio to github.com/adam-kostuch/portfolio.git:gh-pages

---
+++ static/js/main.84ff511a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);var n=c(1),o=c.n(n),r=c(3),s=c.n(r),i=(c(8),c(0)),j=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"Test Object"})})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(j,{})}),document.getElementById("root"))},8:function(t,e,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.84ff511a.chunk.js.map
